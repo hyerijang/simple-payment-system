254f273366f7ed17a6870defd90053dc222fe798
Hyeri Jang <chjang5621@naver.com>
2024-12-03 18:43:39 +0900

    init : Initialize project

---
+++ src/test/java/com/example/simple_payment_system/SimplePaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simple_payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
